7c9b5c4e2d273eb7b2254ac3c0135847616ca3ba
Kamila-Vieira <vkamila54@outlook.com>
2020-07-28 18:47:51 -0300

    Logo da Imersão adicionado no rodapé

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { FooterBase } from './styles';
+import logoMain from '../../assets/img/logoMain.png';
+import logoImersao from '../../assets/img/logoImersao.png';
 
 function Footer() {
   return (
@@ -7,11 +9,15 @@ function Footer() {
       <a href="https://www.alura.com.br/">
         <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
       </a>
+      {/* <p/>
+      <a href="https://tech-dev-flix.vercel.app/">
+        <img src={logoMain} alt="Logo tech.dev.flix" />
+      </a> */}
       <p>
         Orgulhosamente criado durante a
         {' '}
         <a href="https://www.alura.com.br/">
-          Imersão React da Alura
+          <img src={logoImersao} alt="Imersão react da Alura" />
         </a>
       </p>
     </FooterBase>
